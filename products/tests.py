ceaa9b62e5b6af828f57e334d088573ec3aa3508
rafaeldss <rafaeldss199916@gmail.com>
2020-07-12 17:51:39 -0300

    Refatorando tests

--- products/tests.py
+++ products/tests.py
@@ -2,9 +2,11 @@ from django.test import TestCase
 from .models import Product
 
 # Create your tests here.
+
+
 class ProductStrTestCase(TestCase):
     def test_str_should_return_name(self):
         product = Product.objects.create(
-            name="Product test", description="Description test", price=10.90
+            name="Product test", description="Description test", price=10.90, category=1
         )
         assert str(product) == "Name test"
