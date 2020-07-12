838c65af0ac8acf5b366faac42b2d8cead34666a
rafaeldss <rafaeldss199916@gmail.com>
2020-07-12 17:52:11 -0300

    Refatorando tests

--- products/tests.py
+++ products/tests.py
@@ -1,12 +1,17 @@
 from django.test import TestCase
-from .models import Product
+from .models import Product, Category
 
 # Create your tests here.
 
 
 class ProductStrTestCase(TestCase):
     def test_str_should_return_name(self):
+        category = Category.objects.create(
+            name="Category test", description="Description test")
         product = Product.objects.create(
-            name="Product test", description="Description test", price=10.90, category=1
+            name="Product test", description="Description test", price=10.90, category=category
         )
-        assert str(product) == "Name test"
+        result = Product.objects.filter(category=category.id).first()
+
+        expect = "Product test"
+        assert str(result) == expect
